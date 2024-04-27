9e087fcf0488ffa66639bb9eb6c9a2692345a1e6
“MernWithManas” <yesthisismanas@gmail.com>
2024-04-28 02:14:36 +0530

    Added MainLayout and navigation to the application

---
+++ src/pages/Error.jsx
@@ -0,0 +1,14 @@
+
+
+const Error = () => {
+    return (
+        <>
+        <div>
+            You&apos;ve requested an invalid page !
+        </div>
+        </>
+    )
+}
+
+
+export default Error ;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import {Link} from "react-router-dom"
+
+
+const Home = () => {
+    return (
+        <div>
+            <Link to="./Starred">Go to starred page</Link>
+        </div>
+    )
+}
+
+export default Home;
