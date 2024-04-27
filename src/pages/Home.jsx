157854f5c64d29f2ac51bb22752c8db4b31c249d
“MernWithManas” <yesthisismanas@gmail.com>
2024-04-28 04:12:30 +0530

    done pretty much with api call

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,52 @@
-import {Link} from "react-router-dom"
+import { useState } from "react";
+
 
 
 const Home = () => {
+
+const [searchStr, setSearchStr] = useState("");
+const [result, setResult] = useState(null);
+
+console.log(searchStr);
+
+const onSearchInputChange = (ev) => {
+    // console.log(ev.target.value);
+    setSearchStr(ev.target.value)
+}
+
+const onSearch = async (ev) => {
+    ev.preventDefault();
+
+    const result = await fetch(`https://api.tvmaze.com/search/shows?q=${searchStr}`)
+    const setResult =  await result.json()
+
+    console.log(setResult);
+
+}
+
+
+
+
+// https://api.tvmaze.com/search/shows?q=spiderman
+
+
     return (
         <div>
-            <Link to="./Starred">Go to starred page</Link>
+            <form onSubmit={onSearch} >
+
+
+            <input type="text" onChange={onSearchInputChange} />
+            <button type="submit" onClick={onSearch} >Search</button>
+            </form>
+
+            <div>Search Result...</div>
+
+            <ul>
+                {setResult.map((key, data) => {
+                    <li key={key}>{data.name}</li>
+                })}
+            </ul>
+
         </div>
     )
 }
