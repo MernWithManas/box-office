bfe2f0f99d91adf940f1d5c7dba405db14f35f86
“MernWithManas” <yesthisismanas@gmail.com>
2024-04-29 20:10:39 +0530

    showing shows and actors data from api with some error on repeated search

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { searchForShows } from "../api/tvmaze";
+import { searchForShows, searchForPeople} from "../api/tvmaze";
 
 
 const Home = () => {
@@ -7,19 +7,36 @@ const Home = () => {
 const [searchStr, setSearchStr] = useState("");
 const [apiData, setApiData] = useState(null)
 const [apiDataError, setApiDataError] = useState(null)
+const [searchOption, setSearchOption] = useState("shows")
+
+console.log(searchOption);
 
 const onSearchInputChange = (ev) => {
     // console.log(ev.target.value);
     setSearchStr(ev.target.value)
 }
 
+const onRadioChange = (ev) => {
+    setSearchOption(ev.target.value);
+}
+
+
+
 const onSearch = async (ev) => {
     ev.preventDefault();
 
     try {
-        setApiDataError(null)
-        const result = await searchForShows(searchStr);
-        setApiData(result);
+        if (searchOption === "shows") {
+            const result = await searchForShows(searchStr);
+            setApiData(result)
+        } if  (searchOption === "actors") {
+            const result = await searchForShows(searchStr);
+            setApiData(result)
+        } else {
+            const result = await searchForPeople(searchStr);
+            setApiData(result)
+        }
+
     } catch (error) {
         setApiDataError(error);
     }
@@ -31,7 +48,8 @@ const renderApiData = () => {
         return <div>Error Occured: {apiDataError.message}</div>
     }
     if(apiData) {
-        return apiData.map(data => <div key={data.show.id}>{data.show.name}</div>)
+        return apiData[0].show ? apiData.map(data => <div key={data.show.id}>{data.show.name}</div>)
+                            : apiData.map(data => <div key={data.person.id}>{data.person.name}</div>)
     }
 
     return null;
@@ -45,8 +63,18 @@ const renderApiData = () => {
         <div>
             <form onSubmit={onSearch} >
 
-
             <input type="text" onChange={onSearchInputChange} />
+            
+            {/* radio buttons */}
+            <label>Shows
+                <input type="radio" name="search-options" value="shows" onChange={() => setSearchOption === "actors"} checked={searchOption === "shows"} onClick={onRadioChange}/>
+            </label>
+
+            <label>Actors
+                <input type="radio" name="search-options" value="actors" onChange={ () => setSearchOption === "shows"} checked={searchOption === "actors"} onClick={onRadioChange}/>
+            </label>
+
+            {/* submit button */}
             <button type="submit" onClick={onSearch} >Search</button>
             </form>
 
